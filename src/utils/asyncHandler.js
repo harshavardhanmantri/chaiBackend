0fada75aee035c5aaffe168f04f94fbb6dc9e54d
harshavardhanmantri <harshavardhanmantri935@gmail.com>
2024-06-20 22:36:32 +0530

    used utils,cloudinary,routes and middleware

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -13,7 +13,7 @@
 //down one is for promise
 
 const asyncHandler = (requestHandler) => {
-  (req, res, next) => {
+  return (req, res, next) => {
     Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
   };
 };
